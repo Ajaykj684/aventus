d87d83be91bffdac4f5bedf9fc4309dafb024d96
Ajaykj684 <ajaykj684@gmail.com>
2022-11-08 17:28:46 +0530

    APIs done

--- django/users/urls.py
+++ django/users/urls.py
@@ -6,5 +6,12 @@ urlpatterns = [
 
     path('user-login/', views.userLogin, name="user-login"),
     path('add-user/', views.addUser, name="addUser"),
+    path('edit-user/', views.editUser, name="editUser"),
+    path('get-users/', views.getUsers, name="getUsers"),
+    path('search-user/', views.searchUser, name="searchUser"),
+    path('delete-user/', views.deleteUser, name="deleteUser"),
+
+
+
 
 ]
--- django/users/views.py
+++ django/users/views.py
@@ -4,7 +4,9 @@ from rest_framework.response import Response
 from datetime import datetime, timedelta
 from django.conf import settings
 import jwt
-from .models import users
+from .models import users, Designation
+from .serialiser import UserSerializer
+from django.db.models import Q
 
 
 # Create your views here.
@@ -18,23 +20,70 @@ def getAccessToken(payload):
 
 @api_view(['POST'])
 def userLogin(request):
-  return Response("hai im here")
+  return Response('hai im here')
 
 
 @api_view(['POST'])
 def addUser(request):
-  print('111', request.POST.get('email'))
-  email = request.POST['email']
-  print(email)
-  name = request.POST['name']
-  email = request.POST['email']
-  phone = request.POST['phone']
-  password = request.POST['password']
-  address = request.POST['address']
-  designation = request.POST['designation']
+
+  name = request.data['name']
+  email = request.data['email']
+  phone = request.data['phone']
+  password = request.data['password']
+  address = request.data['address']
+  designation = request.data['designation']
 
   try:
-    users.objects.create_user(name=name, email=email, phone=phone, password=password, address=address, designation=designation)
-    return Response({"ok": 1, "msg": "user added succesfully"}, status=200)
+    users.objects.create(name=name, email=email, phone=phone,
+                         password=password, address=address,
+                         designation=Designation.objects.get(designation=designation))
+    return Response({'ok': 1, 'msg': 'user added !'}, status=200)
   except Exception as e:
-    return Response({"ok": 0, "msg": str(e)}, status=400)
+    return Response({'ok': 0, 'msg': str(e)}, status=400)
+
+
+@api_view(['POST'])
+def editUser(request):
+
+  userId = request.data['id']
+  name = request.data['name']
+  email = request.data['email']
+  phone = request.data['phone']
+  password = request.data['password']
+  address = request.data['address']
+  designation = request.data['designation']
+
+  try:
+    users.objects.filter(id=userId).update(name=name, email=email, phone=phone,
+                                           password=password, address=address,
+                                           designation=Designation.objects.get(designation=designation))
+    return Response({'ok': 1, 'msg': 'user updated !'}, status=200)
+  except Exception as e:
+    return Response({'ok': 0, 'msg': str(e)}, status=400)
+
+
+@api_view(['GET'])
+def getUsers(request):
+
+  usersList = users.objects.all()
+  serializeObj = UserSerializer(usersList, many=True)
+  return Response({'ok': 1, 'data': serializeObj.data}, status=200)
+
+
+@api_view(['GET'])
+def searchUser(request):
+
+  keyword = request.data['keyword']
+  usersList = users.objects.filter(Q(name__icontains=keyword) | Q(email=keyword) | Q(designation=keyword))
+  serializeObj = UserSerializer(usersList, many=True)
+  return Response({'ok': 1, 'data': serializeObj.data}, status=200)
+
+
+@api_view(['GET'])
+def deleteUser(request):
+
+  userId = request.data['id']
+  try:
+    users.objects.delete(id=userId)
+  except Exception as e:
+    return Response({'ok': 0, 'msg': str(e)}, status=400)
